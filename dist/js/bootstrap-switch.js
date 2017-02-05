c821695a47197eb8793eec9bbd0b8952d00aa98e
Emanuele <emanuele@lostcrew.it>
2017-02-05 16:10:12 +0100

    Add banner command

--- dist/js/bootstrap-switch.js
+++ dist/js/bootstrap-switch.js
@@ -1,3 +1,12 @@
+/**
+  * bootstrap-switch - Turn checkboxes and radio buttons into toggle switches.
+  *
+  * @version v3.3.2
+  * @homepage http://www.bootstrap-switch.org
+  * @author Mattia Larentis <mattia@larentis.eu> (http://larentis.eu)
+  * @license Apache-2.0
+  */
+
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
