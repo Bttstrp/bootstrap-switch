2b316be8001254ef9f6e229144b625c4674560cb
Anton Sankov <a.sankov96@gmail.com>
2021-01-03 14:50:41 +0200

    Fix typo. Fixes #683 (#700)
    Apparently, this typo leads to a bug with the select button - it is ON, but displayed as OFF, because it is unable to iterate through the classes that muse be set
    
    Co-authored-by: Anton Sankov <Anton.Sankov@paysafe.com>

--- src/js/bootstrap-switch.js
+++ src/js/bootstrap-switch.js
@@ -58,7 +58,7 @@ function prvwidth() {
   return this.$wrapper.width(this.privateHandleWidth + this.privateLabelWidth);
 }
 
-function prvcontainerPosition(state = this.ope) {
+function prvcontainerPosition(state = this.options.state) {
   this.$container.css('margin-left', () => {
     const values = [0, `-${this.privateHandleWidth}px`];
     if (this.options.indeterminate) {
